6d9f07c2ce0e02fb5651c2fae533312fd0f4fdfd
Lorenzo Callegari <Callegari.lorenzo@gmail.com>
2018-10-14 16:26:16 +0200

    Added second assignment

---
+++ assignment_2.py
@@ -0,0 +1,21 @@
+#First assignment for WEEE entry test - Lorenzo Callegari
+from datetime import date
+import datetime
+import calendar
+
+def isRealDate(in_date):
+    try:
+        datetime.datetime.strptime(in_date, '%d/%m/%Y')
+    except ValueError:
+        print("Incorrect data format, should be DD/MM/YYYY")
+        exit()
+
+print("Insert the desired date in the format DD/MM/YYYY")
+in_date = input()
+
+isRealDate(in_date)
+dayName = calendar.day_name[date(int(in_date[6:10]), int(in_date[3:5]), int(in_date[0:2])).weekday()]
+print("\nThe inserted date " + in_date + " is on a " + dayName)
+
+daysFromin_date = str(abs((datetime.datetime(int(in_date[6:10]), int(in_date[3:5]), int(in_date[0:2])) - datetime.datetime.now()).days))
+print(daysFromin_date + " days will pass or have passed from " + in_date)
