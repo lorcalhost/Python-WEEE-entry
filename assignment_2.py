425e940e42a9855e37e6353f78d6a62b4fd7cdb5
Lorenzo Callegari <Callegari.lorenzo@gmail.com>
2018-10-14 17:01:34 +0200

    Fixed second assignment requirements, arguments instead of keyboard input

--- assignment_2.py
+++ assignment_2.py
@@ -1,7 +1,6 @@
 #First assignment for WEEE entry test - Lorenzo Callegari
 from datetime import date
-import datetime
-import calendar
+import datetime, calendar, sys
 
 def isRealDate(in_date):
     try:
@@ -10,8 +9,7 @@ def isRealDate(in_date):
         print("Incorrect data format, should be DD/MM/YYYY")
         exit()
 
-print("Insert the desired date in the format DD/MM/YYYY")
-in_date = input()
+in_date = sys.argv[1]
 
 isRealDate(in_date)
 dayName = calendar.day_name[date(int(in_date[6:10]), int(in_date[3:5]), int(in_date[0:2])).weekday()]
